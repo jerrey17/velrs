be10e6c13ff363bcff85b801e54ddee73a80d2ad
ruijie.zhang <ruijie.zhang@xiaoying.com>
2021-10-08 09:37:13 +0800

    mgnt服务搭建。

---
+++ velrs-mgt/src/main/java/com/velrs/mgt/VelrsMgtApplication.java
@@ -0,0 +1,13 @@
+package com.velrs.mgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelrsMgtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelrsMgtApplication.class, args);
+    }
+
+}
---
+++ velrs-mgt/src/test/java/com/velrs/mgt/MainTest.java
@@ -0,0 +1,14 @@
+package com.velrs.mgt;
+
+/**
+ * @Author rui
+ * @Date 2021-08-12 18:21
+ **/
+public class MainTest {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+
+}
